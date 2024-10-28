0698b3c5d8b0cc2cf7d25dd54c47050f6f3382e9
nino <ninnytcha@gmail.com>
2024-10-28 20:51:04 +0400

    first commit

---
+++ davaleba1.py
@@ -0,0 +1,23 @@
+import d
+
+print('Hello world!')
+# 1
+a = 5
+b = 11
+c = 54
+print(a + b + c)
+
+#2
+ce = 27
+fahrenheit = ce * 9 / 5 + 32
+print(fahrenheit)
+
+#3
+d = input("შეიყვანეთ კუბის გვერდის სიგრძე სანტიმეტრებში: ")
+p = int(d) * 12
+print("კუბის პერიმეტრია", p)
+v = int(d) * int(d) * int(d)
+print("კუბის ფართობია", v)
+
+
+
